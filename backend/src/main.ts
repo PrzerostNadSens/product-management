b7461aec6ac05ee3b3908d1de49006a0692c5b71
Rafał Chmielewski <rafalchmielewski.praca@gmail.com>
2024-06-14 14:01:09 +0200

    feat: add swagger documentation

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,22 @@
-import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
 import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(new ValidationPipe());
+
+  const config = new DocumentBuilder()
+    .setTitle('Product Management API')
+    .setDescription('API for managing products')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
